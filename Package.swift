63be051afd234b3f514ed1982278de671ad0a927
MorganBerger <morgan.berger71@gmail.com>
2026-02-05 15:49:31 +0100

    chore: update Package.swift release checksum for v2.10.1

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "AccessIOS",
-                    url: "https://github.com/p3ol/access-package-ios/releases/download/v2.10.0/AccessIOS.xcframework.zip",
-                    checksum: "6e13bdbc7127c92aa65ef673f14b9dd5f88ee8c96c5735c7a3d638c3601d03ae"
+                    url: "https://github.com/p3ol/access-package-ios/releases/download/v2.10.1/AccessIOS.xcframework.zip",
+                    checksum: "434deabf8248846657b6637994b3d87ddba1d7a85dca2b0db39e13609c19ab24"
                 ),
     ]
 )
